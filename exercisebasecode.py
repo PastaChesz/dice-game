f3791894cbc352b02b3a574eeaf7096b50b3e666
JPMorris510 <joshpowellmorris@gmail.com>
2022-04-06 11:14:29 +0100

    File Addition
    Added the relevant files for the craps task

---
+++ exercisebasecode.py
@@ -0,0 +1,15 @@
+import random
+
+
+def dice():
+    # Write some code that will generate two random numbers up to 6 and add them together
+    return ()
+
+
+def main():
+    _ = input("Press enter to throw the dice")
+    # Write the game code here. You need to compare your dice result against the game rules to determine whether it
+    # has been won, lost or is ongoing.
+
+
+main()
